15e695ec6d6a5d2ed23c6d48b9a22b67fc77b7a9
José G. Quenum <josum@icloud.com>
2021-08-09 12:24:46 +0200

    work in progress

--- tuts/data-analytics.jl
+++ tuts/data-analytics.jl
@@ -8,7 +8,10 @@ using InteractiveUtils
 using PlutoUI, Markdown
 
 # ╔═╡ a97d7008-dd25-4d69-b03a-e4baa072af40
-using CSV, DataFrames, Statistics
+using CSV, DataFrames, Statistics,  Missings,StatsBase
+
+# ╔═╡ 6709d9d3-27d7-4ccd-abbe-397c820ce1d4
+using VegaLite,Dates
 
 # ╔═╡ 55ca5cbf-0961-4fa2-9e35-d8e407587aad
 using BenchmarkTools
@@ -24,7 +27,7 @@ This tutorial will cover the following topics:
 1. Data Exploration
 1. Data Visualisation
 
-Talk briefly about the dataset we will use...
+In this tutorial, we use a dataset related to COVID-19 cases. The dataset presents the number of confirmed, suspected and recovered COVID cases. The observations include national figures as well as regional statistics.
 """
 
 # ╔═╡ f7764099-a07c-448f-b354-9a50a4d90097
@@ -38,14 +41,11 @@ Here we will use packages such as CSV and [DataFrames](https://dataframes.juliad
 # ╔═╡ 470ebfc8-198b-4a65-bb29-38968ac0ef12
 cases_df = DataFrame(CSV.File("data/stateg.csv"))
 
-# ╔═╡ cd58ded6-917c-4628-bfc5-8ac4e43de9fe
-size(cases_df)
-
 # ╔═╡ fb0c3264-0551-4021-a32b-b372b6d379f6
 describe(cases_df, :mean, :std)
 
 # ╔═╡ 5984a29d-4118-41cf-a422-762a4863bdfa
-describe(cases_df)
+describe(cases_df, :all)
 
 # ╔═╡ 386669b6-c0bc-40fa-8a1c-5491788af7e8
 describe(cases_df, cols=1:8)
@@ -73,14 +73,66 @@ with_terminal() do
  @btime cases_df[1:end, 1:end]
 end
 
-# ╔═╡ 9a998c80-0eaa-4bdf-a5a7-8074829b2592
-md"## Cleaning Data"
-
 # ╔═╡ 10f8cbb2-ea14-4a77-9a4c-b080c7f3b5c5
 combine(cases_df, :confirmed => mean => :mean_comfirmed, :suspected => mean => :m_suspected, :level => unique => :u_level)
 
 # ╔═╡ 6ee317b4-d968-4015-a9a4-83f622adea42
 select(cases_df, :confirmed => mean => :mean_comfirmed, :suspected => mean => :m_suspected)
+
+# ╔═╡ 58a8d3ea-cf1a-495b-8b2d-70c76cfc1319
+md"## Cleaning, exploring and visualising Data"
+
+# ╔═╡ 6c7c54fb-4db8-4a7b-942a-af18c97f2757
+nat_df = filter(:level => ==("national"), cases_df)
+
+# ╔═╡ 8b26621f-fbdf-4f69-93a2-2bfe946150f2
+nat_df2 = select(nat_df, Not([:cumulative_quarantined, :region]))
+
+# ╔═╡ 87793ef6-f78c-44b6-87cc-a6069d8cd558
+replace!(nat_df2.total_tested, missing => 0)
+
+# ╔═╡ ecd091f2-526e-4cdc-a6e1-29ae2d3fb2e9
+replace!(nat_df2.total_quarantined, missing => 0)
+
+# ╔═╡ 1a7ef43a-d19c-4d24-8e94-85b98f772b42
+replace!(nat_df2.active_cases, missing => 0)
+
+# ╔═╡ 17a0818a-60f3-40fd-97eb-d6fecc68b1c9
+nat_df2
+
+# ╔═╡ 281fb960-4e6a-4589-bfc7-34bca7a87be3
+nat_df2 |> @vlplot(
+	width=650,
+    height=400,
+	title={text="Confirmed, Acive Cases and Recoveries"},
+	encoding = {
+		x = {field = "date", type = "temporal", timeUnit = "week"}
+	},
+	layer = [
+		{
+			mark = {:line, point = {filled=false, fill=:white, color="#85C5A6"}},
+			y = {field = :confirmed, aggregate = "sum", title = "Confirmed (Week)", type = "quantitative", axis = {titleColor = "#85C5A6"}}
+		},
+		{
+			mark = {:rule, point = {filled=false, fill=:red, color = "#65C5A1"}},
+			y = {field = :active_cases, title = "Active Cases", aggregate = "mean", size = {value = 2}, type = "quantitative"}
+		},
+		{
+			mark = {:circle, point = {filled=false, fill=:purple, color="#85A9C5"}},
+			y = {field = :recovered, aggregate = "sum", title = "Recovered (Week)", type = "quantitative", axis = {titleColor = "#85A9C5"}}
+		}
+	],
+resolve = {scale = {y = "independent"}}
+)
+
+# ╔═╡ cd58ded6-917c-4628-bfc5-8ac4e43de9fe
+size(cases_df)
+
+# ╔═╡ d42190b0-53af-452e-b182-017a8c55b628
+cor(Matrix(nat_df2[:,3:8]))
+
+# ╔═╡ 0c256cea-3f67-4a12-adc3-b59355fcb2c1
+reg_df = filter(:level => ==("regional"), cases_df)
 
 # ╔═╡ 00000000-0000-0000-0000-000000000001
 PLUTO_PROJECT_TOML_CONTENTS = """
@@ -88,15 +140,22 @@ PLUTO_PROJECT_TOML_CONTENTS = """
 BenchmarkTools = "6e4b80f9-dd63-53aa-95a3-0cdb28fa8baf"
 CSV = "336ed68f-0bac-5ca0-87d4-7b16caf5d00b"
 DataFrames = "a93c6f00-e57d-5684-b7b6-d8193f3e46c0"
+Dates = "ade2ca70-3891-5945-98fb-dc099432e06a"
 Markdown = "d6f4376e-aef5-505a-96c1-9c027394607a"
+Missings = "e1d29d7a-bbdc-5cf2-9ac0-f12de2c33e28"
 PlutoUI = "7f904dfe-b85e-4ff6-b463-dae2292396a8"
 Statistics = "10745b16-79ce-11e8-11f9-7d13ad32a3b2"
+StatsBase = "2913bbd2-ae8a-5f71-8c99-4fb6c76f3a91"
+VegaLite = "112f6efa-9a02-5b7d-90c0-432ed331239a"
 
 [compat]
 BenchmarkTools = "~1.1.1"
 CSV = "~0.8.5"
 DataFrames = "~1.2.2"
+Missings = "~0.4.5"
 PlutoUI = "~0.7.9"
+StatsBase = "~0.33.9"
+VegaLite = "~2.6.0"
 """
 
 # ╔═╡ 00000000-0000-0000-0000-000000000002
@@ -130,6 +189,12 @@ git-tree-sha1 = "344f143fa0ec67e47917848795ab19c6a455f32c"
 uuid = "34da2185-b29b-5c13-b0c7-acf172513d20"
 version = "3.32.0"
 
+[[ConstructionBase]]
+deps = ["LinearAlgebra"]
+git-tree-sha1 = "f74e9d5388b8620b4cee35d4c5a618dd4dc547f4"
+uuid = "187b0558-2788-49d3-abe0-74a17ed4e7c9"
+version = "1.3.0"
+
 [[Crayons]]
 git-tree-sha1 = "3f71217b538d7aaee0b69ab47d9b7724ca8afa0d"
 uuid = "a8cc5b0e-0ffa-5ad4-8c14-923d3ee1735f"
@@ -157,6 +222,12 @@ git-tree-sha1 = "bfc1187b79289637fa0ef6d4436ebdfe6905cbd6"
 uuid = "e2d170a0-9d28-54be-80f0-106bbe20a464"
 version = "1.0.0"
 
+[[DataValues]]
+deps = ["DataValueInterfaces", "Dates"]
+git-tree-sha1 = "d88a19299eba280a6d062e135a43f00323ae70bf"
+uuid = "e7dc6d0d-1eca-5fa6-8ad6-5aecde8b7ea5"
+version = "0.4.13"
+
 [[Dates]]
 deps = ["Printf"]
 uuid = "ade2ca70-3891-5945-98fb-dc099432e06a"
@@ -173,6 +244,24 @@ uuid = "8ba89e20-285c-5b6f-9357-94700520ee1b"
 deps = ["ArgTools", "LibCURL", "NetworkOptions"]
 uuid = "f43a241f-c20a-4ad4-852c-f6b1247861c6"
 
+[[FileIO]]
+deps = ["Pkg", "Requires", "UUIDs"]
+git-tree-sha1 = "256d8e6188f3f1ebfa1a5d17e072a0efafa8c5bf"
+uuid = "5789e2e9-d7fb-5bc7-8068-2c6fae9b9549"
+version = "1.10.1"
+
+[[FilePaths]]
+deps = ["FilePathsBase", "MacroTools", "Reexport", "Requires"]
+git-tree-sha1 = "919d9412dbf53a2e6fe74af62a73ceed0bce0629"
+uuid = "8fc22ac5-c921-52a6-82fd-178b2807b824"
+version = "0.8.3"
+
+[[FilePathsBase]]
+deps = ["Dates", "Mmap", "Printf", "Test", "UUIDs"]
+git-tree-sha1 = "0f5e8d0cb91a6386ba47bd1527b240bd5725fbae"
+uuid = "48062228-2e41-5def-b9a4-89aafe57970f"
+version = "0.9.10"
+
 [[Formatting]]
 deps = ["Printf"]
 git-tree-sha1 = "8339d61043228fdd3eb658d86c926cb282ae72a8"
@@ -182,6 +271,18 @@ version = "0.4.2"
 [[Future]]
 deps = ["Random"]
 uuid = "9fa8497b-333b-5362-9e8d-4d0656e87820"
+
+[[HTTP]]
+deps = ["Base64", "Dates", "IniFile", "Logging", "MbedTLS", "NetworkOptions", "Sockets", "URIs"]
+git-tree-sha1 = "44e3b40da000eab4ccb1aecdc4801c040026aeb5"
+uuid = "cd3eb016-35fb-5094-929b-558a96fad6f3"
+version = "0.9.13"
+
+[[IniFile]]
+deps = ["Test"]
+git-tree-sha1 = "098e4d2c533924c921f9f9847274f2ad89e018b8"
+uuid = "83e8ac13-25f8-5344-8a64-a9f2b223428f"
+version = "0.5.0"
 
 [[InteractiveUtils]]
 deps = ["Markdown"]
@@ -203,6 +304,12 @@ deps = ["Dates", "Mmap", "Parsers", "Unicode"]
 git-tree-sha1 = "8076680b162ada2a031f707ac7b4953e30667a37"
 uuid = "682c06a0-de6a-54ab-a142-c8b1cf79cde6"
 version = "0.21.2"
+
+[[JSONSchema]]
+deps = ["HTTP", "JSON", "ZipFile"]
+git-tree-sha1 = "b84ab8139afde82c7c65ba2b792fe12e01dd7307"
+uuid = "7d188eb4-7ad8-530c-ae41-71a32a6d4692"
+version = "0.3.3"
 
 [[LibCURL]]
 deps = ["LibCURL_jll", "MozillaCACerts_jll"]
@@ -230,9 +337,21 @@ uuid = "37e2e46d-f89d-539d-b4ee-838fcccc9c8e"
 [[Logging]]
 uuid = "56ddb016-857b-54e1-b83d-db4d58db5568"
 
+[[MacroTools]]
+deps = ["Markdown", "Random"]
+git-tree-sha1 = "0fb723cd8c45858c22169b2e42269e53271a6df7"
+uuid = "1914dd2f-81c6-5fcd-8719-6d5c9610ff09"
+version = "0.5.7"
+
 [[Markdown]]
 deps = ["Base64"]
 uuid = "d6f4376e-aef5-505a-96c1-9c027394607a"
+
+[[MbedTLS]]
+deps = ["Dates", "MbedTLS_jll", "Random", "Sockets"]
+git-tree-sha1 = "1c38e51c3d08ef2278062ebceade0e46cefc96fe"
+uuid = "739be429-bea8-5141-9913-cc70e7f3736d"
+version = "1.0.3"
 
 [[MbedTLS_jll]]
 deps = ["Artifacts", "Libdl"]
@@ -240,9 +359,9 @@ uuid = "c8ffd9c3-330d-5841-b78e-0817d7145fa1"
 
 [[Missings]]
 deps = ["DataAPI"]
-git-tree-sha1 = "4ea90bd5d3985ae1f9a908bd4500ae88921c5ce7"
+git-tree-sha1 = "f8c673ccc215eb50fcadb285f522420e29e69e1c"
 uuid = "e1d29d7a-bbdc-5cf2-9ac0-f12de2c33e28"
-version = "1.0.0"
+version = "0.4.5"
 
 [[Mmap]]
 uuid = "a63ad114-7e13-5084-954f-fe012c677804"
@@ -252,6 +371,12 @@ uuid = "14a3606d-f60d-562e-9121-12d972cd8159"
 
 [[NetworkOptions]]
 uuid = "ca575930-c2e3-43a9-ace4-1e988b2c1908"
+
+[[NodeJS]]
+deps = ["Pkg"]
+git-tree-sha1 = "905224bbdd4b555c69bb964514cfa387616f0d3a"
+uuid = "2bd173c7-0d6d-553b-b6af-13a54713934c"
+version = "1.3.0"
 
 [[OrderedCollections]]
 git-tree-sha1 = "85f8e6578bf1f9ee0d11e7bb1b1456435479d47c"
@@ -303,6 +428,12 @@ git-tree-sha1 = "5f6c21241f0f655da3952fd60aa18477cf96c220"
 uuid = "189a3867-3050-52da-a836-e630ba90ab69"
 version = "1.1.0"
 
+[[Requires]]
+deps = ["UUIDs"]
+git-tree-sha1 = "4036a3bd08ac7e968e27c203d45f5fff15020621"
+uuid = "ae029012-a4dd-5104-9daa-d747884805df"
+version = "1.1.3"
+
 [[SHA]]
 uuid = "ea8e919c-243c-51af-8825-aaa63cd721ce"
 
@@ -314,6 +445,12 @@ version = "1.3.6"
 
 [[Serialization]]
 uuid = "9e88b42a-f829-5b0c-bbe9-9e923198166b"
+
+[[Setfield]]
+deps = ["ConstructionBase", "Future", "MacroTools", "Requires"]
+git-tree-sha1 = "fca29e68c5062722b5b4435594c3d1ba557072a3"
+uuid = "efcf1570-3423-57d1-acb7-fd33fddbac46"
+version = "0.7.1"
 
 [[SharedArrays]]
 deps = ["Distributed", "Mmap", "Random", "Serialization"]
@@ -336,6 +473,17 @@ uuid = "2f01184e-e22b-5df5-ae63-d93ebab69eaf"
 deps = ["LinearAlgebra", "SparseArrays"]
 uuid = "10745b16-79ce-11e8-11f9-7d13ad32a3b2"
 
+[[StatsAPI]]
+git-tree-sha1 = "1958272568dc176a1d881acb797beb909c785510"
+uuid = "82ae8749-77ed-4fe6-ae5f-f523153014b0"
+version = "1.0.0"
+
+[[StatsBase]]
+deps = ["DataAPI", "DataStructures", "LinearAlgebra", "Missings", "Printf", "Random", "SortingAlgorithms", "SparseArrays", "Statistics", "StatsAPI"]
+git-tree-sha1 = "fed1ec1e65749c4d96fc20dd13bea72b55457e62"
+uuid = "2913bbd2-ae8a-5f71-8c99-4fb6c76f3a91"
+version = "0.33.9"
+
 [[Suppressor]]
 git-tree-sha1 = "a819d77f31f83e5792a76081eee1ea6342ab8787"
 uuid = "fd094767-a336-5f1f-9728-57cf17d0bbfb"
@@ -349,6 +497,12 @@ uuid = "fa267f1f-6049-4f14-aa54-33bafae1ed76"
 deps = ["IteratorInterfaceExtensions"]
 git-tree-sha1 = "c06b2f539df1c6efa794486abfb6ed2022561a39"
 uuid = "3783bdb8-4a98-5b6b-af9a-565f29a5fe9c"
+version = "1.0.1"
+
+[[TableTraitsUtils]]
+deps = ["DataValues", "IteratorInterfaceExtensions", "Missings", "TableTraits"]
+git-tree-sha1 = "8fc12ae66deac83e44454e61b02c37b326493233"
+uuid = "382cd787-c1b6-5bf2-a167-d5b971a19bda"
 version = "1.0.1"
 
 [[Tables]]
@@ -365,12 +519,41 @@ uuid = "a4e569a6-e804-4fa4-b0f3-eef7a1d5b13e"
 deps = ["InteractiveUtils", "Logging", "Random", "Serialization"]
 uuid = "8dfed614-e22c-5e08-85e1-65c5234f0b40"
 
+[[URIParser]]
+deps = ["Unicode"]
+git-tree-sha1 = "53a9f49546b8d2dd2e688d216421d050c9a31d0d"
+uuid = "30578b45-9adc-5946-b283-645ec420af67"
+version = "0.4.1"
+
+[[URIs]]
+git-tree-sha1 = "97bbe755a53fe859669cd907f2d96aee8d2c1355"
+uuid = "5c2747f8-b7ea-4ff2-ba2e-563bfd36b1d4"
+version = "1.3.0"
+
 [[UUIDs]]
 deps = ["Random", "SHA"]
 uuid = "cf7118a7-6976-5b1a-9a39-7adc72f591a4"
 
 [[Unicode]]
 uuid = "4ec0a83e-493e-50e2-b9ac-8f72acf5a8f5"
+
+[[Vega]]
+deps = ["DataStructures", "DataValues", "Dates", "FileIO", "FilePaths", "IteratorInterfaceExtensions", "JSON", "JSONSchema", "MacroTools", "NodeJS", "Pkg", "REPL", "Random", "Setfield", "TableTraits", "TableTraitsUtils", "URIParser"]
+git-tree-sha1 = "43f83d3119a868874d18da6bca0f4b5b6aae53f7"
+uuid = "239c3e63-733f-47ad-beb7-a12fde22c578"
+version = "2.3.0"
+
+[[VegaLite]]
+deps = ["Base64", "DataStructures", "DataValues", "Dates", "FileIO", "FilePaths", "IteratorInterfaceExtensions", "JSON", "MacroTools", "NodeJS", "Pkg", "REPL", "Random", "TableTraits", "TableTraitsUtils", "URIParser", "Vega"]
+git-tree-sha1 = "3e23f28af36da21bfb4acef08b144f92ad205660"
+uuid = "112f6efa-9a02-5b7d-90c0-432ed331239a"
+version = "2.6.0"
+
+[[ZipFile]]
+deps = ["Libdl", "Printf", "Zlib_jll"]
+git-tree-sha1 = "c3a5637e27e914a7a445b8d0ad063d701931e9f7"
+uuid = "a5390f91-8eb1-5f08-bee0-b1d1ffed6cea"
+version = "0.9.3"
 
 [[Zlib_jll]]
 deps = ["Libdl"]
@@ -392,6 +575,7 @@ uuid = "3f19e933-33d8-53b3-aaab-bd5110c3b7a0"
 # ╠═f7764099-a07c-448f-b354-9a50a4d90097
 # ╠═4343fde4-aa4a-41b8-ba1e-68ff6796efed
 # ╠═a97d7008-dd25-4d69-b03a-e4baa072af40
+# ╠═6709d9d3-27d7-4ccd-abbe-397c820ce1d4
 # ╠═470ebfc8-198b-4a65-bb29-38968ac0ef12
 # ╠═cd58ded6-917c-4628-bfc5-8ac4e43de9fe
 # ╠═fb0c3264-0551-4021-a32b-b372b6d379f6
@@ -403,8 +587,17 @@ uuid = "3f19e933-33d8-53b3-aaab-bd5110c3b7a0"
 # ╠═70a0278b-26ac-49ff-b21a-a49ef3d6dec5
 # ╠═55ca5cbf-0961-4fa2-9e35-d8e407587aad
 # ╠═09c11b8a-95c3-4fc6-b481-fd77b1861c18
-# ╠═9a998c80-0eaa-4bdf-a5a7-8074829b2592
 # ╠═10f8cbb2-ea14-4a77-9a4c-b080c7f3b5c5
 # ╠═6ee317b4-d968-4015-a9a4-83f622adea42
+# ╠═58a8d3ea-cf1a-495b-8b2d-70c76cfc1319
+# ╠═6c7c54fb-4db8-4a7b-942a-af18c97f2757
+# ╠═8b26621f-fbdf-4f69-93a2-2bfe946150f2
+# ╠═87793ef6-f78c-44b6-87cc-a6069d8cd558
+# ╠═ecd091f2-526e-4cdc-a6e1-29ae2d3fb2e9
+# ╠═1a7ef43a-d19c-4d24-8e94-85b98f772b42
+# ╠═17a0818a-60f3-40fd-97eb-d6fecc68b1c9
+# ╠═281fb960-4e6a-4589-bfc7-34bca7a87be3
+# ╠═d42190b0-53af-452e-b182-017a8c55b628
+# ╠═0c256cea-3f67-4a12-adc3-b59355fcb2c1
 # ╟─00000000-0000-0000-0000-000000000001
 # ╟─00000000-0000-0000-0000-000000000002
